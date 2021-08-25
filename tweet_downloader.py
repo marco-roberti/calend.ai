74c1a1646eb42168613169dd749c249de1dffd3e
marco\-roberti <m.roberti@unito.it>
2021-08-26 00:04:48 +0200

    add last downloaded tweet's time to progress bar

--- tweet_downloader.py
+++ tweet_downloader.py
@@ -19,21 +19,21 @@ start_time = '2018-06-01T00:00:00+01:00'
 def process_tweet(response_tweet):
     # Ensure this tweet answers to an existing one
     if 'referenced_tweets' not in response_tweet or len(response_tweet['referenced_tweets']) != 1:
-        return None
+        return None, response_tweet['created_at']
     # Get the tweet the current one answers to
     input_tweet = connect_to_endpoint(
         tweet_url + response_tweet['referenced_tweets'][0]['id'], {'tweet.fields': 'author_id'})
     # Ensure this tweet answers to an existing one (again)
     if 'errors' in input_tweet:
-        return None
+        return None, response_tweet['created_at']
     input_tweet = input_tweet['data'][0]
     # Exclude self-answers
     if input_tweet['author_id'] == profile_id:
-        return None
+        return None, response_tweet['created_at']
     # Return actual data
     input_text = input_tweet['text']
     response_text = response_tweet['text']
-    return {'input': input_text, 'output': response_text}
+    return {'input': input_text, 'output': response_text}, response_tweet['created_at']
 
 
 def download_tweets():
@@ -44,16 +44,17 @@ def download_tweets():
     query_params = {
         'query': f'from:{profile} (is:quote OR is:reply) -is:retweet',  # has:hashtags
         'start_time': start_time,
-        'tweet.fields': 'referenced_tweets',
+        'tweet.fields': 'referenced_tweets,created_at',
         'max_results': 100
     }
     pbar = tqdm(desc=profile, unit='Tw')
 
     def process_response():
         for tw in json_response['data']:
-            tw = process_tweet(tw)
+            tw, time = process_tweet(tw)
             if tw is not None:
                 tweets.append(tw)
+                pbar.set_postfix_str(time)
                 pbar.update()
 
     json_response = connect_to_endpoint(search_url, query_params)
