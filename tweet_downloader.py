774410f4161e4a0ae8b00fbe42190f6d40383c19
marco\-roberti <m.roberti@unito.it>
2021-08-25 23:09:34 +0200

    change tasks: inputs are tweets, not hashtags

--- tweet_downloader.py
+++ tweet_downloader.py
@@ -2,23 +2,38 @@ import json
 import os
 import random
 from os import path
-from time import sleep
 
 from tqdm import tqdm
 
 from twitter import connect_to_endpoint
 
 search_url = 'https://api.twitter.com/2/tweets/search/all'
+tweet_url = 'https://api.twitter.com/2/tweets?ids='
 profile = 'CarloCalenda'
+profile_id = '2416067982'
 eval_size = 100
 
 start_time = '2018-06-01T00:00:00+01:00'
 
 
-def process_tweet(tweet):
-    hashtags = ' '.join('#' + ht['tag'] for ht in tweet['entities']['hashtags'])
-    text = tweet['text']
-    return {'input': hashtags, 'output': text}
+def process_tweet(response_tweet):
+    # Ensure this tweet answers to an existing one
+    if 'referenced_tweets' not in response_tweet or len(response_tweet['referenced_tweets']) != 1:
+        return None
+    # Get the tweet the current one answers to
+    input_tweet = connect_to_endpoint(
+        tweet_url + response_tweet['referenced_tweets'][0]['id'], {'tweet.fields': 'author_id'})
+    # Ensure this tweet answers to an existing one (again)
+    if 'errors' in input_tweet:
+        return None
+    input_tweet = input_tweet['data'][0]
+    # Exclude self-answers
+    if input_tweet['author_id'] == profile_id:
+        return None
+    # Return actual data
+    input_text = input_tweet['text']
+    response_text = response_tweet['text']
+    return {'input': input_text, 'output': response_text}
 
 
 def download_tweets():
@@ -27,23 +42,24 @@ def download_tweets():
 
     tweets = []
     query_params = {
-        'query': f'from:{profile} -is:retweet has:hashtags',  # -is:quote -is:reply
+        'query': f'from:{profile} (is:quote OR is:reply) -is:retweet',  # has:hashtags
         'start_time': start_time,
-        'tweet.fields': 'entities',
+        'tweet.fields': 'referenced_tweets',
         'max_results': 100
     }
     pbar = tqdm(desc=profile, unit='Tw')
 
     def process_response():
-        new_tweets = [process_tweet(tw) for tw in json_response['data']]
-        tweets.extend(new_tweets)
-        pbar.update(len(new_tweets))
+        for tw in json_response['data']:
+            tw = process_tweet(tw)
+            if tw is not None:
+                tweets.append(tw)
+                pbar.update()
 
     json_response = connect_to_endpoint(search_url, query_params)
     process_response()
 
     while 'next_token' in json_response['meta']:
-        # sleep(1)  # avoids HTTP response 429 (Too many requests)
         query_params['next_token'] = json_response['meta']['next_token']
         json_response = connect_to_endpoint(search_url, query_params)
         process_response()
