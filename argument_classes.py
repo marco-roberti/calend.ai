959766526e3921c9190af7f7796719215499d15c
marco\-roberti <m.roberti@unito.it>
2021-08-26 19:42:11 +0200

    change max_source_length

--- argument_classes.py
+++ argument_classes.py
@@ -65,7 +65,7 @@ class DataTrainingArguments:
         metadata={"help": "The number of processes to use for the preprocessing."},
     )
     max_source_length: Optional[int] = field(
-        default=128,
+        default=1024,
         metadata={
             "help": "The maximum total input sequence length after tokenization. Sequences longer "
                     "than this will be truncated, sequences shorter will be padded."
