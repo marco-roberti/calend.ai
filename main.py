4e0ce9c2929f72af6d24e59b9abf362212248033
marco\-roberti <m.roberti@unito.it>
2021-08-26 09:34:55 +0200

    print some predictions while evaluating

--- main.py
+++ main.py
@@ -445,6 +445,8 @@ def main():
 
         # Some simple post-processing
         decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+        for pred, label in zip(decoded_preds[:3], decoded_labels[:3]):
+            print(f'Pred: {pred}\nRef:  {label}\n')
 
         result = metric.compute(predictions=decoded_preds, references=decoded_labels)
         result = {"bleu": result["score"]}
