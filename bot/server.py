6bb96b97c97a83add0185d62f1c9c02d0432d4e5
Marco Roberti <m.roberti@unito.it>
2021-09-18 21:32:52 +0200

    rules update

--- bot/server.py
+++ bot/server.py
@@ -9,9 +9,11 @@ def main(args):
     bot = CalendaBot(args, interactive=True)
 
     stream = Stream([
-        {"value": "@Calend_AI -is:retweet -is:reply"},
+        {"value": "@Calend_AI -is:retweet -is:reply -has:links"},
         {"value": "(Calenda OR #CalendaSindaco OR #RomaSulSerio) "
-                  "-is:retweet -is:reply -has:links", "tag": "confirm"}
+                  "-is:retweet -is:reply -has:links", "tag": "confirm_ht"},
+        {"value": "(from:virginiaraggi OR from:gualtierieurope OR from:EnricoMichetti) "
+                  "-is:retweet -is:reply -has:links", "tag": "confirm_cd"}
     ])
     stream.watch(handler=bot.on_quote)
 
